a7a849ce6e5eac5272d0e0ae045494eb55b57e3e
Jorge Castro <jorge.castro.c@gmail.com>
2021-07-03 12:04:41 -0400

    ok.

---
+++ Julio3MVCEF/api/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using Julio3MVCEF.Models;
+using Julio3MVCEF.servicio.repo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Julio3MVCEF.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // api/Values/Listar
+        public List<Customer> Listar()
+        {
+            return CustomerRepo.ListarTodo();
+        }
+    }
+}
